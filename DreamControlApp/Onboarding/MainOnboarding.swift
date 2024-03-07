883aa056fa0d984080e00dc347d23cfbce4a4856
Ainur <sibrinat616@gmail.com>
2024-03-07 05:41:27 +0300

    created onboardingView4

---
+++ DreamControlApp/Onboarding/MainOnboarding.swift
@@ -0,0 +1,25 @@
+//
+//  MainOnboarding.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 05.03.2024.
+//
+
+import SwiftUI
+
+struct MainOnboarding: View {
+    var body: some View {
+        TabView {
+            OnboardingView()
+            OnboardingView2()
+            OnboardingView3()
+
+        }
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+    }
+}
+
+#Preview {
+    MainOnboarding()
+}
--- DreamControlApp/SplashView.swift
+++ DreamControlApp/SplashView.swift
@@ -15,7 +15,7 @@ struct SplashView: View {
     
     var body: some View {
         if isActive {
-            OnboardingView()
+            MainOnboarding()
         } else {
             VStack {
                 VStack {
