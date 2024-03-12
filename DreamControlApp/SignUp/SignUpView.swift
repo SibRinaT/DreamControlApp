a33a923a647724d3f7c19100e7cddf7fd1da76a0
Ainur <sibrinat616@gmail.com>
2024-03-12 19:48:38 +0300

    Created views for login and registration

---
+++ DreamControlApp/SignUp/SignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpView.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 12.03.2024.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpView()
+}
