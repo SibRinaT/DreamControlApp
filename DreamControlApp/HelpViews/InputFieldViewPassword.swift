3bb5f98b63f200e077cd159fd77a3c069d451af1
Ainur <sibrinat616@gmail.com>
2024-03-14 19:33:01 +0300

    input fields created

--- DreamControlApp/HelpViews/InputFieldViewPassword.swift
+++ DreamControlApp/HelpViews/InputFieldViewPassword.swift
@@ -2,14 +2,16 @@
 //  InputFieldView.swift
 //  DreamControlApp
 //
-//  Created by Ainur on 14.03.2024.
+//  Created by Ainur on 13.03.2024.
 //
 
 import SwiftUI
 
-struct InputFieldView: View {
+struct InputFieldViewPassword: View {
     let title: String
     let placeholder: String
+    @State var isSecured = false
+    @State private var isPasswordHidden = true
     @Binding var fieldValue: String
     
     var body: some View {
@@ -19,20 +21,34 @@ struct InputFieldView: View {
                 .font(.caption)
             ZStack(alignment: .trailing) {
                 Group {
-    
+                    if isSecured && isPasswordHidden {
                         SecureField(placeholder, text: $fieldValue)
+                    }
+                    else {
+                        TextField(placeholder, text: $fieldValue)
                     }
                 }
                 .font(.caption2)
                 .textFieldStyle(.roundedBorder)
+                                
+                Button(action: {
+                    isPasswordHidden.toggle()
+                }) {
+                    Image(systemName: self.isPasswordHidden ? "eye" : "eye.slash")
+                        .accentColor(.gray)
+                }
+                .padding(.trailing, 5)
+                .opacity(isSecured ? 1: 0)
+            }
         }
         .padding(.bottom)
     }
 }
 
 #Preview {
-    InputFieldView(title: "Full name",
+    InputFieldViewPassword(title: "Full name",
                    placeholder: "Ivanov Ivan",
+                   isSecured: true,
                    fieldValue:  Binding(
                     get: { "" },
                     set: { _ in }
