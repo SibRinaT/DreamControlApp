e4ce96352572328a73df3e997a5f2cc5d837db61
Ainur <sibrinat616@gmail.com>
2024-04-18 20:28:15 +0300

    just created templates for view

---
+++ DreamControlApp/MainViews/TabBarViews/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 18.04.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
