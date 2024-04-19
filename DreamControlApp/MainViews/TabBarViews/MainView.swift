a2f785c17f5e86172ad033954f23acba86a8cdd4
Ainur <sibrinat616@gmail.com>
2024-04-19 21:15:26 +0300

    main view creating started

--- DreamControlApp/MainViews/TabBarViews/MainView.swift
+++ DreamControlApp/MainViews/TabBarViews/MainView.swift
@@ -9,7 +9,33 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Rectangle()
+                .foregroundColor(Color("PrimaryColor"))
+                .frame(height: 200)
+                .cornerRadius(15)
+                .overlay (
+                    HStack {
+                        Image("IllustrationHome")
+                        VStack {
+                            Text("Привет Айнур")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                                .bold()
+                            Rectangle()
+                                .foregroundColor(.white)
+                                .cornerRadius(15)
+                                .frame(height: 70)
+                                .overlay(
+                                    Text("”Все имеет свою красоту, но не каждый ее видит”")
+                                )
+                            Text("sfgsfgsfg")
+                        }
+                        .padding(.horizontal)
+                    }
+                    )
+        }
+        .padding(.horizontal)
     }
 }
 
