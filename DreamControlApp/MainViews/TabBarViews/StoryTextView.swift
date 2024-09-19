8f151c480d14be323583936a0e0ba429bd4f5225
Ainur <sibrinat616@gmail.com>
2024-09-19 21:03:22 +0300

    storyTextView created

---
+++ DreamControlApp/MainViews/TabBarViews/StoryTextView.swift
@@ -0,0 +1,42 @@
+//
+//  StoryTextView.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 18.09.2024.
+//
+
+import SwiftUI
+
+struct StoryTextView: View {
+    var body: some View {
+        VStack {
+            Rectangle()
+                .foregroundColor(Color("PrimaryColor"))
+                .frame(height: 100)
+                .overlay(
+                    HStack {
+                        Image("CloudForDream")
+                        VStack {
+                            Text("Мечта")
+                                .foregroundColor(Color("InactiveColor2"))
+                                .bold()
+                                .font(.headline)
+                            
+                            Text("dream.name") // change need
+                                .foregroundColor(.white)
+                                .bold()
+                                .font(.title)
+                        }
+                        .padding(.horizontal)
+                        .multilineTextAlignment(.leading)
+                    }
+                )
+            
+            
+        }
+    }
+}
+
+#Preview {
+    StoryTextView()
+}
