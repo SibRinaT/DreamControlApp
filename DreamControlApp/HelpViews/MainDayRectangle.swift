20aa4af25f9c6c3f372f09dfb2caa6d97c595839
Ainur <sibrinat616@gmail.com>
2024-09-28 23:45:45 +0300

    day rectangle start

---
+++ DreamControlApp/HelpViews/MainDayRectangle.swift
@@ -0,0 +1,18 @@
+//
+//  MainDayRectangle.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 28.09.2024.
+//
+
+import SwiftUI
+
+struct MainDayRectangle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainDayRectangle()
+}
