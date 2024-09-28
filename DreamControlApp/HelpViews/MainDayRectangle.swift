58d2cd2f3565d506bfc13d92ed1d4ad61698a73b
Ainur <sibrinat616@gmail.com>
2024-09-28 23:48:40 +0300

    main code pasted

--- DreamControlApp/HelpViews/MainDayRectangle.swift
+++ DreamControlApp/HelpViews/MainDayRectangle.swift
@@ -9,10 +9,44 @@ import SwiftUI
 
 struct MainDayRectangle: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                HelpProfileRectangle()
+                    .overlay (
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Text("Привет Айнур")
+                                    .font(.title)
+                                    .foregroundColor(.white)
+                                    .bold()
+                                    .offset(CGSize(width: 70, height: 10))
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    .cornerRadius(15)
+                                    .frame(width: 200, height: 70)
+                                    .offset(CGSize(width: 70, height: 10))
+                                    .overlay(
+                                        Text("”Все имеет свою красоту, но не каждый ее видит”")
+                                            .gradientForeground(colors: [Color("Prem1"),Color("Prem2"),Color("Prem3")])
+                                            .multilineTextAlignment(.center)
+                                            .offset(CGSize(width: 70, height: 10))
+                                    )
+                                HStack {
+                                    Spacer()
+                                    Text("Гипократ")
+                                        .font(.title2)
+                                        .foregroundColor(.white)
+                                        .bold()
+                                        .offset(CGSize(width: -25, height: 10))
+                                }
+                            }
+                        }
+                    )
+            }
+        }
     }
 }
-
 #Preview {
     MainDayRectangle()
 }
