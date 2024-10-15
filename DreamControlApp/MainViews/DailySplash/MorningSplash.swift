f5f5dbd576b51db6991713d50afbd54141d468d8
Ainur <sibrinat616@gmail.com>
2024-10-15 12:59:47 +0300

    dailySpalsh create start

---
+++ DreamControlApp/MainViews/DailySplash/MorningSplash.swift
@@ -0,0 +1,18 @@
+//
+//  MorningSplash.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 15.10.2024.
+//
+
+import SwiftUI
+
+struct MorningSplash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MorningSplash()
+}
