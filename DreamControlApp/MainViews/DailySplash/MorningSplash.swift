d5403b0328afe5c30a5408f05bc4aeb41f8aa5eb
Ainur <sibrinat616@gmail.com>
2024-10-15 13:08:52 +0300

    morning splash started

--- DreamControlApp/MainViews/DailySplash/MorningSplash.swift
+++ DreamControlApp/MainViews/DailySplash/MorningSplash.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct MorningSplash: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ZStack {
+                Image("dayCircleSplash")
+            }
+            Text("Утро начинается \n не с кофе, а с мечтаний...")
+                .foregroundColor(Color("PrimaryColor"))
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
+            
+        }
     }
 }
 
