db64107ff919b0fdc14961acb12b3712fb88cefe
Ainur <sibrinat616@gmail.com>
2024-10-18 21:00:46 +0300

    first cloud animated

--- DreamControlApp/MainViews/DailySplash/DaySplash.swift
+++ DreamControlApp/MainViews/DailySplash/DaySplash.swift
@@ -8,12 +8,26 @@
 import SwiftUI
 
 struct DaySplash: View {
+    @State private var cloudOffset = CGSize.zero
+
     var body: some View {
         VStack {
             Spacer()
             ZStack {
                 Image("dayCircleSplash")
                     .shadow(radius: 10)
+                Image("Cloud1")
+                    .resizable()
+                    .frame(width: 40, height: 30)
+                    .offset(x: 75, y: -65)
+                    .offset(cloudOffset)
+                                       .animation(.easeInOut(duration: 2), value: cloudOffset) // Анимация перемещения
+                                       .onAppear {
+                                           withAnimation {
+                                               // Укажите здесь целевые координаты, например:
+                                               cloudOffset = CGSize(width: 25, height: 70)
+                                           }
+                                       }
             }
             Text("День - это время  действовать и мечтать!")
                 .foregroundColor(Color("PrimaryColor"))
