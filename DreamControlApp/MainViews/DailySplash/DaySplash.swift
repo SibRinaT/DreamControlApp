5f3d909d2f280ccc86cddd1ebead23344bf3b5e2
Ainur <sibrinat616@gmail.com>
2024-10-18 20:25:11 +0300

    day splash start

---
+++ DreamControlApp/MainViews/DailySplash/DaySplash.swift
@@ -0,0 +1,18 @@
+//
+//  DaySplash.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 18.10.2024.
+//
+
+import SwiftUI
+
+struct DaySplash: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DaySplash()
+}
