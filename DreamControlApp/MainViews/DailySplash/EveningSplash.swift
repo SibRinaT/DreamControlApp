85ee376722bbdf77ebee8765271820cd9c29184b
Ainur <sibrinat616@gmail.com>
2024-10-20 21:11:47 +0300

    first cloud animated

--- DreamControlApp/MainViews/DailySplash/EveningSplash.swift
+++ DreamControlApp/MainViews/DailySplash/EveningSplash.swift
@@ -8,24 +8,32 @@
 import SwiftUI
 
 struct EveningSplash: View {
+    @State private var cloudOffset = CGSize.zero
+    @State private var cloudOffset2 = CGSize.zero
+    @State private var cloudOffset3 = CGSize.zero
+    @State private var starOffset = CGSize.zero
+    @State private var starOffset2 = CGSize.zero
+    @State private var starOffset3 = CGSize.zero
+
+    @State private var rotationCloudAngle: Double = 0
     var body: some View {
         VStack {
             Spacer()
             ZStack {
                 Image("eveningCircleSplash")
                     .shadow(radius: 10)
-//                Image("Cloud1")
-//                    .resizable()
-//                    .frame(width: 40, height: 30)
-//                    .offset(x: 75, y: -65)
-//                    .offset(cloudOffset)
-//                                       .animation(.easeInOut(duration: 2), value: cloudOffset) // Анимация перемещения
-//                                       .onAppear {
-//                                           withAnimation {
-//                                               // Укажите здесь целевые координаты, например:
-//                                               cloudOffset = CGSize(width: 25, height: 70)
-//                                           }
-//                                       }
+                Image("Cloud1")
+                    .resizable()
+                    .frame(width: 40, height: 30)
+                    .offset(x: 75, y: -65)
+                    .offset(cloudOffset)
+                                       .animation(.easeInOut(duration: 2), value: cloudOffset) // Анимация перемещения
+                                       .onAppear {
+                                           withAnimation {
+                                               // Укажите здесь целевые координаты, например:
+                                               cloudOffset = CGSize(width: -50, height: 100)
+                                           }
+                                       }
             }
             Text("Перед сном самое время помечтать")
                 .foregroundColor(Color("SuccessColor"))
