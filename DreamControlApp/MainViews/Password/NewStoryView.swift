0f7f4732bac0f7abe375927d7e51bd50b3858472
Ainur <sibrinat616@gmail.com>
2024-10-25 09:15:18 +0300

    fix autoStory nil check

--- DreamControlApp/MainViews/Password/NewStoryView.swift
+++ DreamControlApp/MainViews/Password/NewStoryView.swift
@@ -12,11 +12,11 @@ struct NewStoryView: View {
     @State private var storyDescription: String = ""
     private let characterLimit = 200
     @State private var autoStory: Bool = false
+
     
-    
-    
+
     var onSave: (String, String) -> Void
-    
+
     var body: some View {
         VStack {
             HStack {
@@ -34,145 +34,125 @@ struct NewStoryView: View {
             // Поле для ввода названия истории
             InputFieldView(title: "Название истории", placeholder: "Введите название", text: $storyTitle)
             
-            // Поле для ввода описания истории
-            if autoStory {
-                VStack(alignment: .leading) {
+                // Поле для ввода описания истории
+                if autoStory {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Spacer()
+                            Text("Описание истории")
+                                .foregroundColor(Color("TextColor"))
+                                .font(.custom("", size: 20)) // need to fix a font
+                            Spacer()
+                        }
+                    }
+                        
+                    ZStack {
+                        Group {
+                            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                                .stroke(Color("PrimaryColor"), lineWidth: 3) // Граница прямоугольника
+                                .background(Color.clear) // Прозрачный фон
+                                .frame(width: 300, height: 200) // Размеры прямоугольника
+                                .padding()
+                            
+                            TextField("Введите описание...", text: $storyDescription, axis: .vertical)
+                                .foregroundColor(Color("TextColor"))
+                                .font(.custom("", size: 16)) // need to fix a font
+                                .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                                .textFieldStyle(PlainTextFieldStyle())
+                                .autocapitalization(.none) // Отключить автоматическое изменение раскладки
+                                .frame(height: 200)
+                                .padding(.horizontal, 40)
+                        }
+                    }
+                    .font(.caption2)
+                    .textFieldStyle(.roundedBorder)
+                    
                     HStack {
                         Spacer()
-                        Text("Описание истории")
-                            .foregroundColor(Color("TextColor"))
-                            .font(.custom("", size: 20)) // need to fix a font
+                        Text("Количество: \(storyDescription.count)/200")
+                            .onChange(of: storyDescription) { newValue in
+                                if newValue.count > characterLimit {
+                                    storyDescription = String(newValue.prefix(characterLimit))
+                                }
+                            }
                         Spacer()
                     }
                 }
-                
-                ZStack {
-                    Group {
-                        RoundedRectangle(cornerRadius: 25, style: .continuous)
-                            .stroke(Color("PrimaryColor"), lineWidth: 3) // Граница прямоугольника
-                            .background(Color.clear) // Прозрачный фон
-                            .frame(width: 300, height: 200) // Размеры прямоугольника
+               
+                VStack {
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            // Переключаем состояние
+                            autoStory.toggle()
+                        }, label: {
+                            HStack {
+                                Circle()
+                                    .strokeBorder(autoStory ? Color("Prem1") : Color.white, lineWidth: 2)
+                                    .background(Circle().fill(autoStory ? Color("Prem1") : Color.clear))
+                                    .frame(width: 20, height: 20)
+                                
+                                Text("Авто-история")
+                                    .font(.title3)
+                                    .foregroundColor(.white)
+                                    .bold()
+                            }
                             .padding()
-                        
-                        TextField("Введите описание...", text: $storyDescription, axis: .vertical)
-                            .foregroundColor(Color("TextColor"))
-                            .font(.custom("", size: 16)) // need to fix a font
-                            .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
-                            .textFieldStyle(PlainTextFieldStyle())
-                            .autocapitalization(.none) // Отключить автоматическое изменение раскладки
-                            .frame(height: 200)
-                            .padding(.horizontal, 40)
+                            .background(Rectangle()
+                                .gradientForeground(colors: [Color("Prem1"),Color("Prem2"),Color("Prem3")])
+                                .cornerRadius(100)
+                                .shadow(radius: 1))
+                        })
+                        Spacer()
                     }
                 }
-                .font(.caption2)
-                .textFieldStyle(.roundedBorder)
+                .padding(.horizontal)
+                .padding(.bottom)
                 
+                // Кнопки "Сохранить" и "Отмена"
+
+                Spacer()
                 HStack {
-                    Spacer()
-                    Text("Количество: \(storyDescription.count)/200")
-                        .onChange(of: storyDescription) { newValue in
-                            if newValue.count > characterLimit {
-                                storyDescription = String(newValue.prefix(characterLimit))
-                            }
-                        }
-                    Spacer()
-                }
-            }
-            
-            VStack {
-                HStack {
-                    Spacer()
                     Button(action: {
-                        // Переключаем состояние
-                        autoStory.toggle()
+                        // Логика для сохранения истории
+                        onSave(storyTitle, storyDescription)
+                        dismiss() // Закрываем окно
                     }, label: {
-                        HStack {
-                            Circle()
-                                .strokeBorder(autoStory ? Color("Prem1") : Color.white, lineWidth: 2)
-                                .background(Circle().fill(autoStory ? Color("Prem1") : Color.clear))
-                                .frame(width: 20, height: 20)
-                            
-                            Text("Авто-история")
-                                .font(.title3)
-                                .foregroundColor(.white)
-                                .bold()
-                        }
-                        .padding()
-                        .background(Rectangle()
-                            .gradientForeground(colors: [Color("Prem1"),Color("Prem2"),Color("Prem3")])
+                        Rectangle()
+                            .foregroundColor(Color("PrimaryColor"))
                             .cornerRadius(100)
-                            .shadow(radius: 1))
+                            .frame(height: 70)
+                            .shadow(radius: 1)
+                            .overlay(
+                                Text("Сохранить")
+                                    .font(.title2)
+                                    .foregroundColor(.white)
+                            )
                     })
-                    Spacer()
+                    .disabled(storyTitle.isEmpty || (autoStory && storyDescription.isEmpty)) // Проверка в зависимости от состояния autoStory
+
+                    Button(action: {
+                        // Закрытие окна без сохранения
+                        dismiss()
+                    }, label: {
+                        Rectangle()
+                            .foregroundColor(Color("PrimaryColor"))
+                            .cornerRadius(100)
+                            .frame(height: 70)
+                            .shadow(radius: 51)
+                            .overlay(
+                                Text("Отмена")
+                                    .font(.title2)
+                                    .foregroundColor(.white)
+                            )
+                    })
                 }
-            }
+
             .padding(.horizontal)
-            .padding(.bottom)
-            
-            // Кнопки "Сохранить" и "Отмена"
-            
-            Spacer()
-            HStack {
-                if autoStory {
-                    Button(action: {
-                        // Логика для сохранения истории
-                        onSave(storyTitle, storyDescription)
-                        dismiss() // Закрываем окно
-                    }, label: {
-                        Rectangle()
-                            .foregroundColor(Color("PrimaryColor"))
-                            .cornerRadius(100)
-                            .frame(height: 70)
-                            .shadow(radius: 1)
-                            .overlay(
-                                Text("Сохранить")
-                                    .font(.title2)
-                                    .foregroundColor(.white)
-                            )
-                    })
-                    .disabled(storyTitle.isEmpty || storyDescription.isEmpty) // Отключить, если поля пустые
-                } else {
-                    Button(action: {
-                        // Логика для сохранения истории
-                        onSave(storyTitle, storyDescription)
-                        dismiss() // Закрываем окно
-                    }, label: {
-                        Rectangle()
-                            .foregroundColor(Color("PrimaryColor"))
-                            .cornerRadius(100)
-                            .frame(height: 70)
-                            .shadow(radius: 1)
-                            .overlay(
-                                Text("Сохранить")
-                                    .font(.title2)
-                                    .foregroundColor(.white)
-                            )
-                    })
-                    .disabled(storyTitle.isEmpty) // Отключить, если поле пустое
-                }
-                
-                Button(action: {
-                    // Закрытие окна без сохранения
-                    dismiss()
-                }, label: {
-                    Rectangle()
-                        .foregroundColor(Color("PrimaryColor"))
-                        .cornerRadius(100)
-                        .frame(height: 70)
-                        .shadow(radius: 51)
-                        .overlay(
-                            Text("Отмена")
-                                .font(.title2)
-                                .foregroundColor(.white)
-                        )
-                })
-            }
+//            Spacer()
         }
-        .padding(.horizontal)
-        //            Spacer()
     }
 }
-
 
 #Preview {
     NewStoryView{_, _ in }
