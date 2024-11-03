6e9e50a2d3e8b65a36f3027bad7712c7d865c62a
Ainur <sibrinat616@gmail.com>
2024-11-03 21:55:11 +0300

    dragging added

--- DreamControlApp/MainViews/IdeaViews/IdeasView.swift
+++ DreamControlApp/MainViews/IdeaViews/IdeasView.swift
@@ -19,22 +19,37 @@ struct IdeasView: View {
                 .foregroundColor(.white)
                 .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 0)
                 .ignoresSafeArea(.all)
-            
                 .overlay(
                     VStack {
-                        Spacer()
                         RoundedRectangle(cornerRadius: 20)
-                            .foregroundColor(.white)
-                            .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 0)
-                            .frame(height: 300)
-                            .padding(.horizontal)
-                            .overlay(
-                                Text(idea)
-                                    .bold()
-                                    .font(.title3)
-                                    .foregroundColor(.black)
-                                    .multilineTextAlignment(.center)
-                            )
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color.black.opacity(0.15), radius: 10, x: 0, y: 0)
+                                        .frame(height: 300)
+                                        .padding(.horizontal)
+                                        .offset(x: dragOffset.width)
+                                        .gesture(
+                                            DragGesture()
+                                                .onChanged { value in
+                                                    dragOffset = value.translation
+                                                }
+                                                .onEnded { value in
+                                                    if value.translation.width > 100 {
+                                                        viewModel.saveIdea(idea, isRightSwipe: true)
+                                                    } else if value.translation.width < -100 {
+                                                        viewModel.saveIdea(idea, isRightSwipe: false)
+                                                    }
+                                                    dragOffset = .zero
+                                                    generateIdea()
+                                                }
+                                        )
+                                        .overlay(
+                                            Text(idea)
+                                                .offset(x: dragOffset.width)
+                                                .bold()
+                                                .font(.title3)
+                                                .foregroundColor(.black)
+                                                .multilineTextAlignment(.center)
+                                        )
                         Spacer()
                         
                         VStack {
