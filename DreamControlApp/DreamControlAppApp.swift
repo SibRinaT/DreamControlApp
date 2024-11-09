1836c9c45c0b86e6b3379a327148c13fe935bb9e
Ainur <sibrinat616@gmail.com>
2024-11-09 10:30:21 +0300

    first launch splash

--- DreamControlApp/DreamControlAppApp.swift
+++ DreamControlApp/DreamControlAppApp.swift
@@ -11,17 +11,47 @@ import SwiftUI
 struct DreamControlAppApp: App {
     private let storiesService: StoriesService
     private let ideasViewModel: IdeasViewModel
+    @State private var isFirstLaunch: Bool
 
     init() {
         storiesService = StoriesService()
         ideasViewModel = IdeasViewModel()
+        
+        // Проверяем, первый ли это запуск
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if !launchedBefore {
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+            _isFirstLaunch = State(initialValue: true)
+        } else {
+            _isFirstLaunch = State(initialValue: false)
+        }
     }
     
     var body: some Scene {
         WindowGroup {
-            SplashView()
+            if isFirstLaunch {
+                SplashView()
+            } else {
+                getTimeBasedSplashView()
+            }
         }
         .environment(storiesService)
-        .environmentObject(ideasViewModel)
+        .environment(ideasViewModel)
+    }
+    
+    // Метод для выбора нужного Splash экрана в зависимости от времени суток
+    private func getTimeBasedSplashView() -> some View {
+        let hour = Calendar.current.component(.hour, from: Date())
+        
+        switch hour {
+        case 6..<12:
+            return AnyView(MorningSplash())
+        case 12..<18:
+            return AnyView(DaySplash())
+        case 18..<24:
+            return AnyView(EveningSplash())
+        default:
+            return AnyView(DaySplash()) // Ночной экран по умолчанию
+        }
     }
 }
