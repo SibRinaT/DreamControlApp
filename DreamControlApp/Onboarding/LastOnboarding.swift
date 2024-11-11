ca4f95f006065adde28f2503af303c3bfa448085
Ainur <sibrinat616@gmail.com>
2024-11-11 20:13:48 +0300

    last onboarding fix

--- DreamControlApp/Onboarding/LastOnboarding.swift
+++ DreamControlApp/Onboarding/LastOnboarding.swift
@@ -17,56 +17,58 @@ struct LastOnboarding: View {
     }
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                VStack {
-                    Image("registrImage")
-                    Image("registrImage")
-                    Image("registrImage")
-                }
-                Rectangle()
-                    .frame(height: 590)
-                    .cornerRadius(50)
-                    .shadow(radius: 30, y: -4)
-                    .overlay(
-                        VStack {
-                            Text("Регистрация")
-                                .padding(.top, 30)
-                                .font(.largeTitle)
-                                .bold()
-                                .foregroundColor(Color("TextColor"))
-                            Rectangle()
-                                .frame(height: 10) // Spacer
-                            
-                            InputFieldView(title: "Email", placeholder: "DreamControl@gmail.com", text: $email)
-                            InputFieldView(title: "Имя", placeholder: "Dream", text: $name)
-                            
-                            NavigationLink(destination: CustomTabBar(), isActive: $navigateToTabBar) {
-                                EmptyView()
-                            }
-                            
-                            YellowButtonLastOnboard(isActive: isButtonActive, text: "Войти") {
-                                if isButtonActive {
-                                    // Обновляем флаг онбординга в UserDefaults
-                                    UserDefaults.standard.set(true, forKey: "onboardingCompleted")
-                                    navigateToTabBar = true // Активируем навигацию
-                                }
-                            }
-                            
-                            Rectangle()
-                                .frame(height: 10) // Spacer
-                           
-                            Rectangle()
-                                .frame(height: 30) // Spacer
-                            
-                            Spacer()
-                            Image("FullLogo")
-                                .padding(.bottom, 30)
-                        }
-                    )
-                    .foregroundColor(.white)
-                    .offset(y: 120)
+        ZStack {
+            VStack {
+                Image("registrImage")
+                Image("registrImage")
+                Image("registrImage")
+                Image("registrImage")
+                Image("registrImage")
+                
             }
+            Rectangle()
+                .frame(height: 590)
+                .cornerRadius(50)
+                .shadow(radius: 30, y: -4)
+                .overlay(
+                    VStack {
+                        Text("Регистрация")
+                            .padding(.top, 30)
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(Color("TextColor"))
+                        Rectangle()
+                            .frame(height: 10) // Spacer
+                        
+                        InputFieldView(title: "Email", placeholder: "DreamControl@gmail.com", text: $email)
+                        InputFieldView(title: "Имя", placeholder: "Dream", text: $name)
+                        
+                        NavigationLink(destination: CustomTabBar(), isActive: $navigateToTabBar) {
+                            EmptyView()
+                        }
+                        .navigationBarHidden(true) 
+                        
+                        YellowButtonLastOnboard(isActive: isButtonActive, text: "Войти") {
+                            if isButtonActive {
+                                // Обновляем флаг онбординга в UserDefaults
+                                UserDefaults.standard.set(true, forKey: "onboardingCompleted")
+                                navigateToTabBar = true // Активируем навигацию
+                            }
+                        }
+                        
+                        Rectangle()
+                            .frame(height: 10) // Spacer
+                        
+                        Rectangle()
+                            .frame(height: 30) // Spacer
+                        
+                        Spacer()
+                        Image("FullLogo")
+                            .padding(.bottom, 50)
+                    }
+                )
+                .foregroundColor(.white)
+                .offset(y: 120)
         }
     }
 }
