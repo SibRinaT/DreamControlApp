c457b4802b35ac8cbbe018666b29f2450acf889b
Ainur <sibrinat616@gmail.com>
2024-11-12 11:11:27 +0300

    create big yellow button for more clean code

---
+++ DreamControlApp/HelpViews/BigYellowButton.swift
@@ -0,0 +1,37 @@
+//
+//  BigYellowButton.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 12.11.2024.
+//
+
+import SwiftUI
+
+struct BigYellowButton: View {
+    @State var title: String
+    @State var image: String
+
+    var body: some View {
+        Button(action: {
+        }) {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundColor(Color("PrimaryColor")) // Прозрачный фон
+                .frame(height: 85)
+                .overlay (
+                    HStack {
+                        Image(image)
+                            .padding()
+                        Text(title)
+                            .bold()
+                            .foregroundColor(.white)
+                            .font(.title2)
+                            .padding()
+                    }
+                )
+        }
+    }
+}
+
+#Preview {
+    BigYellowButton(title: "Виджеты", image: "StoryIcon")
+}
