d79cfe11f76ccec50885a653810de5dec4ac55cd
Ainur <sibrinat616@gmail.com>
2024-11-12 13:58:53 +0300

    cloudes offsets fix

--- DreamControlApp/HelpViews/MainView/HelpProfileRectangleEvening.swift
+++ DreamControlApp/HelpViews/MainView/HelpProfileRectangleEvening.swift
@@ -26,7 +26,7 @@ struct HelpProfileRectangleEvening: View {
                         Image("CloudImage")
                             .offset(CGSize(width: -150, height: 65))
                         Image("CloudImage")
-                            .offset(CGSize(width: -40, height: 60))
+                            .offset(CGSize(width: -50, height: 60))
                         Image("StarSmallImage")
                             .offset(CGSize(width: -40, height: 30))
                         Image("StarSmallImage")
