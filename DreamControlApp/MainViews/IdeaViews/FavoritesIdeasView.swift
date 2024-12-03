c2bf8ab37607ce335139e1c132bc78b1c51bf0f9
Ainur <sibrinat616@gmail.com>
2024-12-03 11:27:28 +0300

    FavoritesIdeasView in extra file

---
+++ DreamControlApp/MainViews/IdeaViews/FavoritesIdeasView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesIdeasView.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 03.12.2024.
+//
+
+import SwiftUI
+
+struct FavoritesIdeasView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesIdeasView()
+}
