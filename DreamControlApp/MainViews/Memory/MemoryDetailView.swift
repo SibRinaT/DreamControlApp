45ddc1eb63addfd3c6bf466077e9406139f9f0e6
Ainur <sibrinat616@gmail.com>
2025-05-08 19:30:50 +0300

    add new groups and created memory detail view

---
+++ DreamControlApp/MainViews/Memory/MemoryDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryDetailView.swift
+//  DreamControlApp
+//
+//  Created by Ainur on 08.05.2025.
+//
+
+import SwiftUI
+
+struct MemoryDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoryDetailView()
+}
